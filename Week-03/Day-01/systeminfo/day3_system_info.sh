1bda1f4b9527940a3f2124f322619e80561f2492
imtiyaj <imtiyaj@gmail.com>
2026-07-08 19:45:11 +0530

    fix: corrected uptime backslash bug in day3 script

--- Week-03/Day-01/systeminfo/day3_system_info.sh
+++ Week-03/Day-01/systeminfo/day3_system_info.sh
@@ -4,7 +4,7 @@
 # Script Name : day3_system_info.sh
 # Description : Tracks basic system health (Hostname, Uptime, Memory)
 # Author      : Imtiyaj
-# Date        : 2026-07-05
+# Date        : 2026-07-08
 # ====================================================================
 
 echo "=========================================="
@@ -14,8 +14,8 @@ echo "=========================================="
 # 1. Hostname (System ka naam)
 echo "➜ Hostname: $(hostname)"
 
-# 2. System Uptime (System kab se chal raha hai)
-echo "➜ System Uptime: \$(uptime -p 2>/dev/null || echo 'N/A on Windows Git Bash')"
+# 2. System Uptime (Isme se backslash '\' hata diya hai taaki AWS par sahi chale)
+echo "➜ System Uptime: $(uptime -p 2>/dev/null || echo 'N/A on Windows Git Bash')"
 
 # 3. Available Memory (RAM check)
 echo "➜ Memory Details:"
